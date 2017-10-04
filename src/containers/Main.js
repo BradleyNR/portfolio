a31b7aa6b1090f3ed05205bf7d485bb06a255dac
Bradley_Carson <bmcarson1671@gmail.com>
2017-10-04 00:41:08 -0400

    initial

---
+++ src/containers/Main.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class Main extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return(
+      <div className='row'>
+        <header className='main-header'>
+          <h1 className='main-header-text'>Bradley Carson</h1>
+        </header>
+        <div className='testing'>
+
+        </div>
+      </div>
+
+    )
+  }
+}
+
+export default Main;
---
+++ src/layout/base.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Main from '../containers/Main';
+
+function BaseLayout(){
+  return(
+    <div>
+      <Main />
+    </div>
+  )
+}
+
+
+export default BaseLayout;
