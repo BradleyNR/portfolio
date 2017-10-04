fa56b0402916e38d98ca0a142142b4bf54efc8a2
Bradley_Carson <bmcarson1671@gmail.com>
2017-10-04 15:20:25 -0400

    working

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+import skills from '../data/skills.js';
+import js from '../images/js_logo.png';
+
 class Main extends Component{
   constructor(props){
     super(props);
@@ -10,18 +13,30 @@ class Main extends Component{
   }
 
   render(){
+    let skillBox = skills.map((item, index) => {
+      return (
+        <div className='skill-box three columns offset-by-one' key={item.id}>
+          {/* <img src={require(self.image)} className='skill-image'/> */}
+
+          <h1 className='skill-title'>{item.title}</h1>
+        </div>
+      )
+    })
+
     return(
       <div className='row'>
         <header className='main-header'>
           <h1 className='main-header-text'>Bradley Carson</h1>
+          <h2 className='main-header-secondary-text'>Front End Developer</h2>
         </header>
-        <div className='testing'>
-
+        <div className='skill-area-bg'>
+          {skillBox}
         </div>
       </div>
 
     )
   }
 }
+
 
 export default Main;
---
+++ src/data/skills.js
@@ -0,0 +1,7 @@
+let skills = [
+  {id: 1, title: 'React', img:'../images/react_logo.svg'},
+  {id: 2, title: 'JavaScript', img: '../images/js_logo.png'},
+  {id: 3, title: 'Add-Ons', img: ''}
+];
+
+export default skills;
