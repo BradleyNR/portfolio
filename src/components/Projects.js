d43abfa4c8ff3484b9b7e4d728f3ae590c8d5480
Bradley_Carson <bmcarson1671@gmail.com>
2017-10-08 22:38:04 -0400

    added gifs, updated project section with links and updated styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,17 +7,18 @@ class Projects extends Component{
 
     let projectBox = projects.map((item, index) => {
       return (
-        <div className='project-box five columns offset-by-one' key={index}>
-          <div className='project-img-container'>
+        <div className='project-box twelve columns row' key={index}>
+          <div className='project-img-container five columns offset-by-one'>
             <a href={item.url}> <img src={item.img} className='project-img' alt='project'/> </a>
           </div>
 
 
-          <div className='row'>
+          <div className='five columns offset-by-one'>
             <h1 className='project-title twelve columns'>{item.title}</h1>
           </div>
-          <div className='row'>
-            <p className='skill-description'>{item.description}</p>
+          <div className='five columns offset-by-one'>
+            <p className='project-description'>{item.description}</p>
+            <p className='project-description-codelink'>Click the image to see the project live, or click here to see the code on <a href={item.githubUrl}>Github!</a></p>
           </div>
         </div>
       )
