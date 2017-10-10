e9b072f8dd1770e986b853062c31bd1104718d17
Bradley_Carson <bmcarson1671@gmail.com>
2017-10-10 19:11:02 -0400

    minor styling changes

--- src/components/References.js
+++ src/components/References.js
@@ -4,7 +4,7 @@ class References extends Component {
   render(){
     return (
       <div className='reference-area-bg row'>
-        <h1 className='references-header' id='references'>References</h1>
+        <h1 className='references-header' id='references'>Testimonial</h1>
         <h6 className='eight columns offset-by-two quotation'>"Brad is an intelligent, motivated individual with a thirst for knowledge. His analytical skills allow him to look at a problem logically and get down to the root cause efficiently. He doesn't settle for just knowing "what" - he wants to know the how and why, which during his time working for me allowed him to obtain a far above average understanding of the systems we supported. He never hesitates to help colleagues and is genuinely interested in contributing to the team and larger organization. I would not hesitate to bring him back on to my team and would highly recommend him for any software project."</h6>
         <h5 className='six columns offset-by-six'>- <a href='https://www.linkedin.com/in/chris-cranford-00ab7493/'>Chris Cranford</a>, SCDHHS</h5>
       </div>
