69db733c6b058a39cf021c38b336001639b3720a
Bradley_Carson <bmcarson1671@gmail.com>
2017-10-10 12:10:28 -0400

    reworked project area and made responsive

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,15 +8,12 @@ class Projects extends Component{
     let projectBox = projects.map((item, index) => {
       return (
         <div className='project-box twelve columns row' key={index}>
-          <div className='project-img-container six columns'>
+          <h1 className='project-title twelve columns'>{item.title}</h1>
+          <div className='project-img-container twelve columns'>
             <a href={item.url}> <img src={item.img} className='project-img' alt='project'/> </a>
           </div>
 
-
-          <div className='five columns offset-by-one'>
-            <h1 className='project-title twelve columns'>{item.title}</h1>
-          </div>
-          <div className='five columns offset-by-one'>
+          <div className='ten columns offset-by-one'>
             <p className='project-description'>{item.description}</p>
             <p className='project-description-codelink'>Click the image to see the project live, or click here to see the code on <a href={item.githubUrl}>Github!</a></p>
           </div>
