835a4c7f4a5b9ee9dcbe1ee755b93eff283b9345
Bradley_Carson <bmcarson1671@gmail.com>
2017-10-17 00:15:02 -0400

    added responsiveness to mobile view

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,21 +1,21 @@
 let projects = [
   {
     title: 'Whisper',
-    img: '/images/whisper_gif.gif',
+    img: '/portfolio/images/whisper_gif.gif',
     url: 'https://whisper-app.herokuapp.com/login',
     description: 'Whisper is a faux-social media webapp made with Node.js! Create an account, sign in, and create some whispers!',
     githubUrl: 'https://github.com/BradleyNR/weekly-7-gabble'
   },
   {
     title: 'Photofall',
-    img: '/images/photofall_gif.gif',
+    img: '/portfolio/images/photofall_gif.gif',
     url: 'https://bradleynr.github.io/10.2-photofall/',
     description: 'Photofall is an open imageboard website created with React! Post photos in real time for other users to see!',
     githubUrl: 'https://github.com/BradleyNR/10.2-photofall'
   },
   {
     title: 'Calcubaker',
-    img: '/images/calcubaker_gif.gif',
+    img: '/portfolio/images/calcubaker_gif.gif',
     url: 'https://bradleynr.github.io/weekly-11-bakers/',
     description: 'Calcubaker is a webapp created with React for recipe storage and adjustment. Create an account, then a recipe. Alter the serving size and the ingredient amounts will update in real time!',
     githubUrl: 'https://github.com/BradleyNR/weekly-11-bakers'
--- src/data/skills.js
+++ src/data/skills.js
@@ -2,37 +2,37 @@ let skills = [
   {
     id: 1,
     title: 'React',
-    img: '/images/react_logo.png',
+    img: '/portfolio/images/react_logo.png',
     description: "I enjoy creating responsive, modern websites with React. No one likes to wait, so delivering a snappy, clean website experience is my goal."
   },
   {
     id: 2,
     title: 'JavaScript',
-    img: '/images/js_logo.png',
+    img: '/portfolio/images/js_logo.png',
     description: "I love coding out clever solutions to difficult problems, and JavaScript allows me to do just that. I enjoy bringing interesting ideas to life from scratch, learning new tips and tricks along the way."
   },
   {
     id: 3,
     title: 'CSS',
-    img: '/images/css_logo.png',
+    img: '/portfolio/images/css_logo.png',
     description: "I am familiar with CSS as well as popular frameworks like Bootstrap. Clever use of CSS can turn a website into a wonderful user experience!"
   },
   {
-    id: 3,
+    id: 4,
     title: 'Node.js',
-    img: '/images/nodejs_logo.svg',
+    img: '/portfolio/images/nodejs_logo.svg',
     description: "Having worked with Node.js, I have a good understanding of back end fundamentals and principles."
   },
   {
-    id: 3,
+    id: 5,
     title: 'Express',
-    img: '/images/express_logo.png',
+    img: '/portfolio/images/express_logo.png',
     description: "I also have experience with Express.js creating backend webapps and custom APIs"
   },
   {
-    id: 3,
+    id: 6,
     title: 'Databases',
-    img: '/images/database_logo.jpg',
+    img: '/portfolio/images/database_logo.jpg',
     description: "Having worked with SQL (PostgreSQL) and NoSQL (MongoDB) databases has made me comfortable with integrating and using databases into different projects!"
   }
 ];
