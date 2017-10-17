79df59a86008e9575a4b08f24981e83ffa5d0702
Bradley_Carson <bmcarson1671@gmail.com>
2017-10-16 23:56:09 -0400

    restyled project area

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,16 +7,19 @@ class Projects extends Component{
 
     let projectBox = projects.map((item, index) => {
       return (
-        <div className='project-box twelve columns row' key={index}>
-          <h1 className='project-title twelve columns'>{item.title}</h1>
+        <div className='project-box three columns offset-by-one' key={index}>
+          {/* <h1 className='project-title twelve columns'>{item.title}</h1> */}
           <div className='project-img-container twelve columns'>
             <a href={item.url}> <img src={item.img} className='project-img' alt='project'/> </a>
           </div>
-
+          <div className='twelve columns'>
+            <p className='project-description-codelink button'><a href={item.githubUrl}>View Code</a></p>
+          </div>
+{/*
           <div className='ten columns offset-by-one'>
             <p className='project-description'>{item.description}</p>
             <p className='project-description-codelink'>Click the image to see the project live, or click here to see the code on <a href={item.githubUrl}>Github!</a></p>
-          </div>
+          </div> */}
         </div>
       )
     });
@@ -24,6 +27,7 @@ class Projects extends Component{
     return (
       <div className='project-area-bg'>
         <h1 className='projects-header' id='projects-area-main-header'>Projects</h1>
+        <p className='project-header-subtext'>Click on the photo to view live, or click View Code to see the code on Github!</p>
         {projectBox}
       </div>
     )
--- src/data/skills.js
+++ src/data/skills.js
@@ -2,37 +2,37 @@ let skills = [
   {
     id: 1,
     title: 'React',
-    img: '/portfolio/images/react_logo.png',
+    img: '/images/react_logo.png',
     description: "I enjoy creating responsive, modern websites with React. No one likes to wait, so delivering a snappy, clean website experience is my goal."
   },
   {
     id: 2,
     title: 'JavaScript',
-    img: '/portfolio/images/js_logo.png',
+    img: '/images/js_logo.png',
     description: "I love coding out clever solutions to difficult problems, and JavaScript allows me to do just that. I enjoy bringing interesting ideas to life from scratch, learning new tips and tricks along the way."
   },
   {
     id: 3,
     title: 'CSS',
-    img: '/portfolio/images/css_logo.png',
+    img: '/images/css_logo.png',
     description: "I am familiar with CSS as well as popular frameworks like Bootstrap. Clever use of CSS can turn a website into a wonderful user experience!"
   },
   {
     id: 3,
     title: 'Node.js',
-    img: '/portfolio/images/nodejs_logo.svg',
+    img: '/images/nodejs_logo.svg',
     description: "Having worked with Node.js, I have a good understanding of back end fundamentals and principles."
   },
   {
     id: 3,
     title: 'Express',
-    img: '/portfolio/images/express_logo.png',
+    img: '/images/express_logo.png',
     description: "I also have experience with Express.js creating backend webapps and custom APIs"
   },
   {
     id: 3,
     title: 'Databases',
-    img: '/portfolio/images/database_logo.jpg',
+    img: '/images/database_logo.jpg',
     description: "Having worked with SQL (PostgreSQL) and NoSQL (MongoDB) databases has made me comfortable with integrating and using databases into different projects!"
   }
 ];
